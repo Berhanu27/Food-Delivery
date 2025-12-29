78654cc4577dda0b4741824329cefa0ba807881e
Berhanu27 <berhanumulu2022@gmail.com>
2025-12-29 23:11:36 +0300

    Use env variable for MongoDB URI

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://brexman:bermul2727@cluster0.ohe8ost.mongodb.net/food-delivery')
+    await mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://brexman:bermul2727@cluster0.ohe8ost.mongodb.net/food-delivery')
     .then(() => console.log("DB Connected"))
     .catch((err) => console.log("DB Connection Error:", err));
 };
